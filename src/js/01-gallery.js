61308c4bac6608fbab9ff4361f49a461b51da580
Kostya9911 <koshka5557@i.ua>
2022-12-25 17:52:20 +0200

    gallery-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 
-console.log(galleryItems);
+console.log(SimpleLightbox);
+
+// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+
+const listGallery = galleryItems.map(
+  ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__item" class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"      
+      alt="${description}"      
+    />
+  </a>  
+</div>`
+);
+gallery.innerHTML = listGallery.join(' ');
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
